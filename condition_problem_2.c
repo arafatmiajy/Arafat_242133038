4f4d96298e1bae34913677d7e3700aacfbf61ed0
arafatmiajy <mdarafatmiajy555@gmail.com>
2025-02-05 22:22:07 +0600

    condition_problem_2.c

--- condition_problem_2.c
+++ condition_problem_2.c
@@ -1,12 +1,14 @@
 #include<stdio.h>
 
-int main(){
-int number;
-printf("Give your number: ");
-scanf("%d",&number);
-if(number % 2 ==0){
-printf("The number is evan");}
-else {
-printf("The number is odd");}
-return 0;
+int main() {
+    int number;
+    printf("Give your number: ");
+    scanf("%d",&number);
+    if(number % 2 ==0) {
+        printf("The number is evan");
+    }
+    else {
+        printf("The number is odd");
+    }
+    return 0;
 }
